438230c39f2e8de3937a437c867247446a8d7497
shun-cheng <shuncheng.wu91@gmail.com>
2020-11-28 13:28:50 +0100

    fix eigne link issue

--- cmake/LinkEigen3.cmake
+++ cmake/LinkEigen3.cmake
@@ -3,5 +3,6 @@
 ####################
 
 if(EIGEN3_INCLUDE_DIR)
-  target_include_directories(${targetname} ${EIGEN3_INCLUDE_DIR})
+  target_include_directories(${targetname} PUBLIC ${EIGEN3_INCLUDE_DIR})
+  TARGET_COMPILE_DEFINITIONS(${targetname} PUBLIC COMPILE_WITH_EIGEN)
 endif(EIGEN3_INCLUDE_DIR)
